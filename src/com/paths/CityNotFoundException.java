39e6f3b084668159e74023386c6b3577afcda5e0
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-03-12 23:04:07 +0530

    v8.0 | v8.0 is implemented. Paths.java is modified. new methods are added such as sortByCost() implemented.

--- src/com/paths/CityNotFoundException.java
+++ src/com/paths/CityNotFoundException.java
@@ -1,9 +1,14 @@
 package com.paths;
 
 class CityNotFoundException extends Exception {
-	String message;
+	private String message;
 
 	CityNotFoundException(String city) {
 		this.message = "No City names \"" + city + "\" in Database";
 	}
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
